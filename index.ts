b9dda2c7389445675f4dae63dfb362a163ff6141
Rowan C <rowan.dev.email@gmail.com>
2023-11-27 17:46:31 -0800

    move test script and default exports

--- index.ts
+++ index.ts
@@ -1,27 +1,15 @@
 import Element from './src/Element.js';
+import Node from './src/Node.js';
 import Screen from './src/Screen.js';
 
-const scr = new Screen();
-const el = new Element({
-    fg: 'red',
-    bg: 'green',
-    content: '{center}{magenta-fg}hello world{/magenta-fg}{/center}{left}{blue-bg}left{/blue-bg}{/left}\n{right}right{/right}\n{red-bg}left seperator{|}right seperator{/red-bg}',
-    screen: scr,
-    width: '50%',
-    height: '50%',
-    left: '10%',
-    top: '10%',
-});
-scr.render();
-scr.key(['esc*', 'q', 'C-c'], () => {
-    process.exit();
-});
-scr.key('h', () => {
-    el.height = '40%';
-    scr.render();
-});
-scr.key('u', () => {
-    el.height = '50%';
-    scr.render();
-});
+export {
+    Element,
+    Node,
+    Screen
+}
 
+export default {
+    Element,
+    Node,
+    Screen
+}
---
+++ test.ts
@@ -0,0 +1,25 @@
+import nd from './index.js'
+const scr = new nd.Screen();
+const el = new nd.Element({
+    fg: 'red',
+    bg: 'green',
+    content: '{center}{magenta-fg}hello world{/magenta-fg}{/center}{left}{blue-bg}left{/blue-bg}{/left}\n{right}right{/right}\n{red-bg}left seperator{|}right seperator{/red-bg}',
+    screen: scr,
+    width: '50%',
+    height: '50%',
+    left: '10%',
+    top: '10%',
+});
+scr.render();
+scr.key(['esc*', 'q', 'C-c'], () => {
+    process.exit();
+});
+scr.key('h', () => {
+    el.height = '40%';
+    scr.render();
+});
+scr.key('u', () => {
+    el.height = '50%';
+    scr.render();
+});
+
